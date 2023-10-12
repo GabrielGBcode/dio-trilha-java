6e19328087adfa8fd2171d2718342858cc8a5ef7
Gabriel GB <105217214+GabrielGBcode@users.noreply.github.com>
2023-10-11 23:46:26 -0300

    Entregando desafio

---
+++ src/Contador.java
@@ -0,0 +1,39 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.println("Digite um número inteiro: ");
+            int num1 = scanner.nextInt();
+
+            System.out.println("Digite outro número inteiro: ");
+            int num2 = scanner.nextInt();
+
+            try {
+                contar(num1, num2);
+            } catch (Exception e) {
+                System.out.println("O segundo parâmentro deve ser maior que o primeiro");
+            }
+        } catch (Exception e) {
+            System.out.println("Digite um número inteiro.");
+        }
+
+    }
+
+    static void contar(int parm1, int parm2) throws ParametrosInvalidosException {
+        if (parm1 > parm2) {
+            throw new ParametrosInvalidosException();
+        } else {
+            int contagem = parm2 - parm1;
+            for (int i = 0; i < contagem; i++) {
+                System.out.println("Imprimindo o número " + (i + 1));
+            }
+
+        }
+
+    }
+}
